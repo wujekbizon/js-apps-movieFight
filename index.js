3b042dc831561e446ebd70627bc0871028d1d4ee
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-05-19 11:13:19 +0200

    small style change

--- index.js
+++ index.js
@@ -79,10 +79,10 @@ const runComparison = () => {
     const rightSideValue = parseFloat(rightStat.dataset.value);
 
     if (rightSideValue > leftSideValue) {
-      leftStat.classList.remove('is-primary');
+      leftStat.classList.remove('is-success');
       leftStat.classList.add('is-warning');
     } else {
-      rightStat.classList.remove('is-primary');
+      rightStat.classList.remove('is-success');
       rightStat.classList.add('is-warning');
     }
   });
@@ -119,23 +119,23 @@ const movieTemplate = (movieDetail) => {
             </div> 
         </div>
         </article>
-        <article data-value=${awards} class="notification is-primary">
+        <article data-value=${awards} class="notification is-success">
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article data-value=${dollars} class="notification is-primary">
+        <article data-value=${dollars} class="notification is-success">
             <p class="title">${movieDetail.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article data-value=${metascore} class="notification is-primary">
+        <article data-value=${metascore} class="notification is-success">
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article data-value=${imdbRating} class="notification is-primary">
+        <article data-value=${imdbRating} class="notification is-success">
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article data-value=${imdbVotes} class="notification is-primary">
+        <article data-value=${imdbVotes} class="notification is-success">
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>

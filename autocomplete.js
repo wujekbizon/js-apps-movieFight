2010ae049ec9bd612ba7efc6952a8b1e5f356c16
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-05-19 09:34:11 +0200

    Initial commit

---
+++ autocomplete.js
@@ -0,0 +1,53 @@
+const createAutoComplete = ({
+    root,
+    renderOption,
+    onOptionSelect,
+    inputValue,
+    fetchData
+}) => {
+
+    root.innerHTML = `
+        <label><b>Search</b></label>
+         <input class="input" />
+         <div class="dropdown">
+             <div class="dropdown-menu">
+                 <div class="dropdown-content results"></div>
+             </div>
+         </div>
+    `;
+
+    const input = root.querySelector('input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
+
+    const onInput = async event => {
+        const items = await fetchData(event.target.value);
+
+        if (!items.length) {
+            dropdown.classList.remove('is-active');
+            return;
+        }
+        resultsWrapper.innerHTML = '';
+        dropdown.classList.add('is-active');
+        for (let item of items) {
+            const option = document.createElement('a')
+
+            option.classList.add('dropdown-item');
+            option.innerHTML = renderOption(item);
+            option.addEventListener('click', () => {
+                input.value = inputValue(item);
+                dropdown.classList.remove('is-active');
+                onOptionSelect(item);
+            });
+            resultsWrapper.appendChild(option);
+        };
+    };
+
+    input.addEventListener('input', debounce(onInput, 600));
+
+    document.addEventListener('click', event => {
+        if (!root.contains(event.target)) {
+            dropdown.classList.remove('is-active')
+        }
+    });
+};
